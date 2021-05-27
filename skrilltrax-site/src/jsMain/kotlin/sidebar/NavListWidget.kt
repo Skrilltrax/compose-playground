413759238b36d4fe78b2d4e54cd297d5efa38a91
Aditya Wasan <adityawasan55@gmail.com>
2021-05-27 21:19:02 +0530

    feat: use custom column widget
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- skrilltrax-site/src/jsMain/kotlin/sidebar/NavListWidget.kt
+++ skrilltrax-site/src/jsMain/kotlin/sidebar/NavListWidget.kt
@@ -1,15 +1,36 @@
 package sidebar
 
 import androidx.compose.runtime.Composable
+import androidx.compose.web.css.*
+import androidx.compose.web.elements.Div
 import navigation.Destination
+import org.jetbrains.compose.common.foundation.layout.Column
 import org.jetbrains.compose.common.foundation.layout.Row
 import org.jetbrains.compose.common.material.Text
+import org.jetbrains.compose.common.ui.Modifier
 
 @Composable
 fun NavListWidget(destinations: List<Destination>, selectedDestination: Destination) {
-  for (destination in destinations) {
+  /*for (destination in destinations) {
     Row {
       Text(destination.name)
     }
+  }*/
+
+  CustomColumn {
+    Text("AAAAA")
+    Text("BBBBBBBBBB")
+  }
+}
+
+@Composable
+fun CustomColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+  Div(
+    style = {
+      display(DisplayStyle.Flex)
+      flexDirection(FlexDirection.Column)
+    }
+  ) {
+    content()
   }
 }
